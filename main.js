112e0a8d251afc05a47d29c216b0f6bd62d9a238
nmprogramming <48452623+nmprogramming@users.noreply.github.com>
2019-04-29 15:05:45 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 var length = document.getElementById("length")
 var complexity = document.getElementsByName("complexity")
 var result = document.getElementById('result');
+var previous = document.getElementById('previous')
+
+var passwords = [];
+
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 letters = ['a', 'b' ,'c' ,'d' ,'e' ,'f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 symbols = ['!', '@', '#', '$', '%']
@@ -18,6 +22,18 @@ function getRandomCharacter(array)
   var index = Math.floor(Math.random() * array.length);
   var char = array[index];
   return char;
+}
+
+function render()
+{
+  previous.innerText = "";
+  var localPasswords = JSON.parse(window.localStorage.getItem("passwords"));
+  for(var i = 0; i < localPasswords.length; i++)
+  {
+    previous.innerHTML += "<li>" + localPasswords[i] + "</li>";
+  }
+
+  console.log(window.localStorage.getItem("passwords"))
 }
 
 function generate()
@@ -57,7 +73,19 @@ function generate()
     password += charToAdd
   }
 
+  passwords.push(password);
+  window.localStorage.setItem("passwords", JSON.stringify(passwords));
+
+  render();
+
   return password;
+}
+
+function onClearPrevious()
+{
+  passwords = [];
+  window.localStorage.setItem("passwords", JSON.stringify(passwords));
+  render();
 }
 
 function onClick()
