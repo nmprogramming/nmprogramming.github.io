a91778424cee2c51915d2db8dbaf3c62b57d62f1
nmprogramming <48452623+nmprogramming@users.noreply.github.com>
2019-04-29 15:07:47 -0400

    Update main.js

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 letters = ['a', 'b' ,'c' ,'d' ,'e' ,'f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 symbols = ['!', '@', '#', '$', '%']
 
+render();
+
 function getNumberInRange(min, max)
 {
   var minimum = Math.ceil(min);
