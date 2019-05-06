1755fa6630ec4d9f401bafb65613f99bd870107d
nmprogramming <48452623+nmprogramming@users.noreply.github.com>
2019-05-06 14:08:51 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ var result = document.getElementById('result');
 var previous = document.getElementById('previous')
 
 var passwords = [];
+if(window.localStorage.getItem("passwords") != "")
+{
+  passwords = JSON.parse(window.localStorage.getItem("passwords"));
+}
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 letters = ['a', 'b' ,'c' ,'d' ,'e' ,'f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
